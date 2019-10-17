905063163dd808b2bc79481031525f38d6e596e3
Michael Benedetti <benedettim@forcenexus.io>
2019-10-17 06:42:27 -0400

    Added explosive attack

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public GameObject explosion;
+    public float radius = 5.0F;
+    public float power = 50.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+                RaycastHit hit;
+                
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                    GameObject newExplosion = (GameObject) Instantiate(explosion, hit.point, Quaternion.identity);
+                    Collider[] colliders = Physics.OverlapSphere(hit.point, radius);
+                    foreach (Collider collide in colliders)
+                    {
+                        Rigidbody rb = collide.GetComponent<Rigidbody>();
+                        if (rb != null)
+                         rb.AddExplosionForce(power, hit.point, radius, 3.0F, ForceMode.Impulse);
+                    }
+                    Destroy(newExplosion, 1);
+                }
+            }
+    }
+}
